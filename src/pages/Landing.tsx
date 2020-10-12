638c2fb6ab62e154a15165dfabb61f3d6fb21882
Bruno Honda <bruno.honda@4linux.com.br>
2020-10-12 16:31:55 -0300

    Criada tela inicial e roteamento

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { FiArrowRight } from 'react-icons/fi';
+
+import '../styles/pages/landing.css';
+import logoImage from '../images/logo.svg';
+
+export default function Landing() {
+    return (
+        <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImage} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o munfdo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Poços de Caldas</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <Link className="enter-app" to="/app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+      </div>
+    </div>
+    );
+}
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+// import '../styles/pages/orphanages-map.css';
+import logoImage from '../images/logo.svg';
+
+export default function OrphanagesMap() {
+    return (
+      <div id="page-orphanages-map">
+        
+      </div>
+    );
+}
